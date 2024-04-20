369029750b91b87029c4f41cfce63ba0b2bcdae8
M4ALV3S <marcos_14alves@outlook.com>
2024-04-20 16:32:25 -0300

    Feat adicionando validações na classe Abrigo

--- ConexaoAnimal/Models/Abrigo.cs
+++ ConexaoAnimal/Models/Abrigo.cs
@@ -6,9 +6,12 @@ public class Abrigo {
   public int AbrigoId {  get; set; }
 
   [Required]
+  [StringLength(30, ErrorMessage = "O número máxino deve ser 30 caracteres")]
+  [MinLength(3, ErrorMessage = "O número mínimo deve ser de 3 caracteres")]
   public string? Nome {  get; set; }
-  
+
   [Required]
+  [Range(0, 1000, ErrorMessage = "O abrigo deve conter de 0 ou até 1000 Pets")]
   public int QtdPets {  get; set; }
   
   public int EnderecoId {  get; set; }
