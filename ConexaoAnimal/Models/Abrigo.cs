d65d060f8bb182f4e711536a328f2b77fba3d51f
Samuel Lima <samuel.lima@inteliger.com.br>
2024-04-21 02:39:17 -0300

    - Criação de tabela Tutores; - Refatoração de código; - Exclusão propriedade abrigo na tabela de endereço;  - Usando Data Annotations para referenciar chaves nas colunas;  - Indentação;

--- ConexaoAnimal/Models/Abrigo.cs
+++ ConexaoAnimal/Models/Abrigo.cs
@@ -1,21 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Abrigo {
+  public DateTime DataCriacao { get; private set; } = DateTime.UtcNow;
 
+  [Key]
   [Required]
-  public int AbrigoId {  get; set; }
+  public int AbrigoId { get; set; }
 
   [Required]
   [StringLength(30, ErrorMessage = "O número máxino deve ser 30 caracteres")]
   [MinLength(3, ErrorMessage = "O número mínimo deve ser de 3 caracteres")]
-  public string? Nome {  get; set; }
+  public string? Nome { get; set; }
 
   [Required]
   [Range(0, 1000, ErrorMessage = "O abrigo deve conter de 0 ou até 1000 Pets")]
-  public int QtdPets {  get; set; }
-  
-  public int EnderecoId {  get; set; }
-  public Endereco Endereco { get;  set; }
-  
+  public int QtdPets { get; set; }
 
+  [ForeignKey("Endereco")]
+  public int EnderecoId { get; set; }
+  public Endereco Endereco { get; set; }
 }
--- ConexaoAnimal/Models/Endereco.cs
+++ ConexaoAnimal/Models/Endereco.cs
@@ -1,38 +1,38 @@
 using System.ComponentModel.DataAnnotations;
 
 public class Endereco {
+  public DateTime DataCriacao { get; private set; } = DateTime.UtcNow;
 
-  [Required] 
+  [Key]
+  [Required]
   public int EnderecoId { get; set; }
 
   [Required]
   [StringLength(30, ErrorMessage = "O campo deve conter no máximo 30 caracteres")]
   [MinLength(6, ErrorMessage = "O campo deve conter no mínimo 6 caracteres")]
-  public string?  Logradouro { get; set; }
+  public string? Logradouro { get; set; }
 
   [Required]
   [StringLength(35, ErrorMessage = "O campo deve conter no máximo 35 caracteres")]
   [MinLength(2, ErrorMessage = "O campo deve conter no mínimo 6 caracteres")]
-  public string? Cidade {  get; set; }
+  public string? Cidade { get; set; }
 
   [Required]
   [StringLength(2, ErrorMessage = "O campo deve conter no máximo 2 caracteres")]
   [MinLength(2, ErrorMessage = "O campo deve conter no mínimo 2 caracteres")]
-  public string? Uf {  get; set; }
+  public string? Uf { get; set; }
 
   [Required]
   [RegularExpression(@"^\d{5}-\d{3}$", ErrorMessage = "Por favor, insira um CEP válido no formato 12345-678.")]
   [StringLength(9, ErrorMessage = "O campo deve conter no máximo 9 caracteres")]
   [MinLength(9, ErrorMessage = "O campo deve conter no mínimo 9 caracteres")]
-  public string? Cep {  get; set; }
+  public string? Cep { get; set; }
 
   [Required]
   [Range(0, 5000, ErrorMessage = "O número deve estar entre 0 e 5000")]
-    public int Numero {  get; set; }
+  public int Numero { get; set; }
 
   [StringLength(70, ErrorMessage = "O campo deve conter no máximo 70 caracteres")]
   [MinLength(5, ErrorMessage = "O campo deve conter no mínimo 5 caracteres")]
-  public string?  Complemento {  get; set; }
-  public Abrigo? Abrigo { get; set; }
-  
+  public string? Complemento { get; set; }
 }
