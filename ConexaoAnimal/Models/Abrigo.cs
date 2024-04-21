778c55fe89b1bdec917aeafa14b43128c3464d7b
heroshg <heros.goodoy@gmail.com>
2024-04-21 13:50:53 -0300

    update tabelas e relacionamentos

--- ConexaoAnimal/Models/Abrigo.cs
+++ ConexaoAnimal/Models/Abrigo.cs
@@ -2,22 +2,18 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 public class Abrigo {
-  public DateTime DataCriacao { get; private set; } = DateTime.UtcNow;
-
-  [Key]
   [Required]
   public int AbrigoId { get; set; }
 
-  [Required]
+  [Required(ErrorMessage = "Campo Nome deve ser preenchido")]
   [StringLength(30, ErrorMessage = "O número máxino deve ser 30 caracteres")]
   [MinLength(3, ErrorMessage = "O número mínimo deve ser de 3 caracteres")]
   public string? Nome { get; set; }
 
-  [Required]
+  [Required(ErrorMessage = "Campo QtdPets deve ser preenchido")]
   [Range(0, 1000, ErrorMessage = "O abrigo deve conter de 0 ou até 1000 Pets")]
   public int QtdPets { get; set; }
-
-  [ForeignKey("Endereco")]
+  public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
   public int EnderecoId { get; set; }
-  public Endereco? Endereco { get; set; }
+  public Endereco Endereco { get; set; }
 }
--- ConexaoAnimal/Models/Endereco.cs
+++ ConexaoAnimal/Models/Endereco.cs
@@ -1,38 +1,39 @@
 using System.ComponentModel.DataAnnotations;
 
 public class Endereco {
-  public DateTime DataCriacao { get; private set; } = DateTime.UtcNow;
-
-  [Key]
   [Required]
   public int EnderecoId { get; set; }
 
-  [Required]
+  [Required(ErrorMessage = "Campo Logradouro deve ser preenchido")]
   [StringLength(30, ErrorMessage = "O campo deve conter no máximo 30 caracteres")]
   [MinLength(6, ErrorMessage = "O campo deve conter no mínimo 6 caracteres")]
   public string? Logradouro { get; set; }
 
-  [Required]
+  [Required(ErrorMessage = "Campo Cidade deve ser preenchido")]
   [StringLength(35, ErrorMessage = "O campo deve conter no máximo 35 caracteres")]
   [MinLength(2, ErrorMessage = "O campo deve conter no mínimo 6 caracteres")]
   public string? Cidade { get; set; }
 
-  [Required]
+  [Required(ErrorMessage = "Campo UF deve ser preenchido")]
   [StringLength(2, ErrorMessage = "O campo deve conter no máximo 2 caracteres")]
   [MinLength(2, ErrorMessage = "O campo deve conter no mínimo 2 caracteres")]
   public string? Uf { get; set; }
 
-  [Required]
+  [Required(ErrorMessage = "Campo CEP deve ser preenchido")]
   [RegularExpression(@"^\d{5}-\d{3}$", ErrorMessage = "Por favor, insira um CEP válido no formato 12345-678.")]
   [StringLength(9, ErrorMessage = "O campo deve conter no máximo 9 caracteres")]
   [MinLength(9, ErrorMessage = "O campo deve conter no mínimo 9 caracteres")]
   public string? Cep { get; set; }
 
-  [Required]
+  [Required(ErrorMessage = "Campo Número deve ser preenchido")]
   [Range(0, 5000, ErrorMessage = "O número deve estar entre 0 e 5000")]
   public int Numero { get; set; }
 
   [StringLength(70, ErrorMessage = "O campo deve conter no máximo 70 caracteres")]
   [MinLength(5, ErrorMessage = "O campo deve conter no mínimo 5 caracteres")]
   public string? Complemento { get; set; }
+  public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+  public Abrigo? Abrigo { get; set; }
+  public Tutor? Tutor { get; set; }
+
 }
